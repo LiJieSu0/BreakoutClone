61c0ba28aa5cd15487918fca1bf437009bffbace
LiJieSu0 <opwinopwin@gmail.com>
2024-09-14 15:51:33 -0500

    add facing direction

--- Script/Basic/Player.cs
+++ Script/Basic/Player.cs
@@ -4,15 +4,13 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	#region Nodes
-	[Export] private Node2D Core;
-	private Path2D path;
-    private PathFollow2D pathFollow;
+
 	private Ball _mainBall;
     private Node _ballManager;
+	private PathFollow2D pathFollow;
 	#endregion
 	
 	#region Variables
-	private Vector2 _centerPosition;
 	private float _raidus=100f;
 	private float angle = 0f; 
 	public const float Speed = 300.0f;
@@ -26,47 +24,27 @@ public partial class Player : CharacterBody2D
 		InitializeNode();
 		InitializeVariables();
 		InitializeSignal();
-        path = new Path2D();
-		Core.AddChild(path);
-
-        // 創建 PathFollow2D 節點
-        pathFollow = new PathFollow2D();
-        path.AddChild(pathFollow);
-
-        // 創建一個圓形路徑
-        CreateCircularPath();
-		pathFollow.AddChild(this);
 
 	}
 	public override void _PhysicsProcess(double delta)
 	{
-		if(Input.IsActionJustPressed("ui_left")){
+		if(Input.IsActionPressed("ui_left")){
 			pathFollow.ProgressRatio-=(float)delta;
 		}
-		if(Input.IsActionJustPressed("ui_right")){
+		if(Input.IsActionPressed("ui_right")){
 			pathFollow.ProgressRatio+=(float)delta;
 		}
 	}
-    private void CreateCircularPath()
-    {
-        var curve = new Curve2D();
-        for (int i = 0; i < segments; i++)
-        {
-            float angle = 2*Mathf.Pi * i / segments; 
-            Vector2 point = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _raidus;
-            curve.AddPoint(point);
-        }
-        path.Curve = curve;
-    }
+
 	private void InitializeNode(){
 		// _mainBall=GetNode<Ball>("Ball");
 		// _ballManager=GetParent().GetNode("BallManager");
+		pathFollow=this.GetParent<PathFollow2D>();
 	}
 	private void InitializeSignal(){
 		ReceiveEffectEvent+=PlayerReceiveEffect;
 	}
 	private void InitializeVariables(){
-		_centerPosition=Core.GlobalPosition;
 	}
     public void ReceiveEffectTrigger(ItemEffect effect){
 		ReceiveEffectEvent.Invoke(effect);
@@ -83,23 +61,6 @@ public partial class Player : CharacterBody2D
 		}
     }
 
-	private void ReparentNode(Node2D node, Node2D newParent)
-    {
-        // 確認節點當前的父節點
-        Node currentParent = node.GetParent();
-
-        if (currentParent != null)
-        {
-            // 從當前父節點中移除
-            currentParent.RemoveChild(node);
-        }
-
-        // 添加到新的父節點
-        newParent.AddChild(node);
-
-        // 如果需要，可以設置新的變換（如位置）
-        node.GlobalPosition = newParent.GlobalPosition;
-    }
 	private void OrignalBreakoutCloneMove(){
 		Vector2 velocity = Velocity;
 		if (Input.IsActionJustPressed("ui_accept") &&GameManager.Instance.gameState==GameState.GameReady){//Space key pressed
---
+++ Script/Levels/SceneManager.cs
@@ -0,0 +1,82 @@
+using Godot;
+using System;
+
+public partial class SceneManager : Node2D
+{
+	#region Nodes
+	[Export] PackedScene _playerScene;
+	[Export] private Node2D Core;
+	private Path2D path;
+    private PathFollow2D pathFollow;
+	private Line2D line2D;
+    #endregion
+
+    #region Variables
+    [Export] private int segments=10;
+    [Export] private float _raidus=100f;
+	private Vector2 _centerPoint;
+    #endregion
+
+    // Called when the node enters the scene tree for the first time.
+
+    public override void _Ready()
+	{
+		InitializeNode();
+		InitializeVariables();
+		line2D = new Line2D();
+		path = new Path2D();
+		AddChild(path);
+		CreateCircularPath();
+        pathFollow = new PathFollow2D();
+        path.AddChild(pathFollow);
+		Player _player=_playerScene.Instantiate<Player>();
+		pathFollow.AddChild(_player);
+		pathFollow.ProgressRatio=0;
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 facingDirection = new Vector2( Mathf.Sin(pathFollow.Rotation),Mathf.Cos(pathFollow.Rotation));
+	}
+
+	private void CreateCircularPath(){
+        var curve = new Curve2D();
+		Vector2 origin=Vector2.Zero;
+        for (int i = 0; i < segments; i++)
+        {
+
+            float angle = 2*Mathf.Pi * i / segments; 
+            Vector2 point = _centerPoint+new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * _raidus;
+            curve.AddPoint(point);
+			if(i==0)
+				origin=point;
+        }
+		curve.AddPoint(origin);
+        path.Curve = curve;
+		for (int i = 0; i < curve.GetPointCount(); i++)
+        {
+            line2D.AddPoint(curve.GetPointPosition(i));
+        }
+
+        // Customize the Line2D appearance (optional)
+        line2D.Width = 2;
+        line2D.DefaultColor = Colors.Red;
+		AddChild(line2D);
+    }
+
+	private void InitializeNode(){
+	
+	}
+	
+	private void InitializeSignal(){
+	
+	}
+	
+	private void InitializeVariables(){
+		_centerPoint=Core.GlobalPosition;
+	
+	}
+
+}
